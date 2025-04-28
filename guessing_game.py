3d67c221552d653738ed33fecc9363e1d5f819c0
sayuni02 <41-bit-0052@kdu.ac.lk>
2025-04-28 20:25:09 +0530

    Change range to 1-50

---
+++ guessing_game.py
@@ -0,0 +1,8 @@
+import random 
+number = random.randint(1, 50) 
+print("Guess a number between 1 and 50") 
+guess = int(input()) 
+if guess == number: 
+    print("You win!") 
+else: 
+    print(f"Wrong! The number was {number}") 
